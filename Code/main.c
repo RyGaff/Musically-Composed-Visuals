3a76a90d9d8c38bca92d8f092097d74e9f66d71d
Ryan Gaffney <rtgaff3@gmail.com>
2023-04-04 11:57:39 -0400

    Added functionality to animate, press space to enable and disable

--- Code/main.c
+++ Code/main.c
@@ -13,6 +13,34 @@ int height;
 int width;
 int MAX_ITERATIONS = 500;
 
+double zoom = 1;
+double mx = 0;
+double my = 0;
+
+int animation = 0;
+
+double random_interval();
+void display();
+void animate();
+void julia(double zoom, double mX, double mY);
+void key_listener(unsigned char key, int x, int y);
+double random_interval() {return (double)rand()/(double)RAND_MAX;}
+
+int main( int argc, char** argv )
+{
+    glutInit( &argc, argv );
+    glutInitDisplayMode( GLUT_DOUBLE | GLUT_RGBA );
+    glutInitWindowSize( 1000, 1000);
+    glutInitWindowPosition(950,0);
+    glutCreateWindow( "Julia" );
+
+    glutDisplayFunc( display );
+    glutIdleFunc(animate);
+    glutKeyboardFunc(key_listener);
+    glutMainLoop();
+    return 0;
+}
+
 void julia(double zoom, double mX, double mY)
 {
     
@@ -37,7 +65,8 @@ void julia(double zoom, double mX, double mY)
 				if((zx * zx + zy * zy) > 4) break;
 			}	
 				if(iteration == MAX_ITERATIONS ){
-                glColor3f( 1.0, 1.0, 1.0 ); // Set color to draw julia
+                // glColor3f( 1.0, 1.0, 1.0 ); // Set color to draw julia
+                glColor3f(0.5, 1.0, 1.0); // Set color to draw julia
                 glVertex2i( x, y );
 			}
             else {
@@ -47,6 +76,14 @@ void julia(double zoom, double mX, double mY)
 		}
 	}
     glEnd();
+}
+
+void animate(){
+    if (animation == 1){
+        cRe = sin(cRe);
+        cIm = cos(cIm);
+    }
+    display();
 }
 
 void display()
@@ -63,7 +100,7 @@ void display()
     glLoadIdentity();
 
     //TODO Allow the (zoom, mx, my) parameters to be changed via user input or some other method
-    julia( 1.0, 0.0, 0.0);
+    julia(zoom, mx, my);
     glutSwapBuffers();
 }
 
@@ -89,26 +126,21 @@ void key_listener(unsigned char key, int x, int y){
             break;
         //Adjust Imaginary
         case 'i':
-            cIm += .1;
+            cIm += .01;
             break;
         case 'k':
-            cIm -= .1;
+            cIm -= .01;
+            break;
+        case 32: // Spacebar
+            if (animation == 0){
+                animation = 1;
+            } else {
+                animation = 0;
+            }
             break;
     }
-    printf("Iterations %d   real = %f    imaginary = %f\n", MAX_ITERATIONS, cRe, cIm);
-
+    printf("Iterations %d   real = %f    imaginary = %f     animation = %d\n", MAX_ITERATIONS, cRe, cIm, animation);
 }
 
-int main( int argc, char** argv )
-{
-    glutInit( &argc, argv );
-    glutInitDisplayMode( GLUT_DOUBLE | GLUT_RGBA );
-    glutInitWindowSize( 1000, 1000);
-    glutCreateWindow( "Julia" );
 
-    glutDisplayFunc( display );
-    glutIdleFunc(display);
-    glutKeyboardFunc(key_listener);
-    glutMainLoop();
-    return 0;
-}
+
